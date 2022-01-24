956ed74462b2b192d3c728be5f216013125f8686
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2022-01-24 17:06:18 +0100

    Simple Unit Tests

---
+++ tests/OutingUnitTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Outing;
+use PHPUnit\Framework\TestCase;
+use DateTime;
+
+class OutingUnitTest extends TestCase
+{
+    public function testIsTrue(): void
+    {
+        $outing = new Outing();
+
+        $dateTime = new DateTime();
+        $interval = date_interval_create_from_date_string('10 days');
+
+        $outing->setName('Musée')
+            ->setStartAt($dateTime)
+            ->setDuration($interval)
+            ->setLimitSubscriptionDate($dateTime)
+            ->setMaxUsers(15)
+            ->setAbout('10 jours au musée, oui c\'est long');
+
+
+        $this->assertTrue($outing->getName() === 'Musée');
+        $this->assertTrue($outing->getStartAt() === $dateTime);
+        $this->assertTrue($outing->getDuration() === $interval);
+        $this->assertTrue($outing->getLimitSubscriptionDate() === $dateTime);
+        $this->assertTrue($outing->getMaxUsers() === 15);
+        $this->assertTrue($outing->getAbout() === '10 jours au musée, oui c\'est long');
+    }
+
+    public function testIsFalse(): void
+    {
+        $outing = new Outing();
+
+        $dateTime = new DateTime();
+        $dateFalse = date_create('2022-04-01');
+        $interval = date_interval_create_from_date_string('10 days');
+        $intervalFalse = date_interval_create_from_date_string('10 weeks');
+
+
+        $outing->setName('Musée')
+            ->setStartAt($dateTime)
+            ->setDuration($interval)
+            ->setLimitSubscriptionDate($dateTime)
+            ->setMaxUsers(15)
+            ->setAbout('10 jours au musée, oui c\'est long');
+
+        $this->assertFalse($outing->getName() === 'Beuverie');
+        $this->assertFalse($outing->getStartAt() === $dateFalse);
+        $this->assertFalse($outing->getDuration() === $intervalFalse);
+        $this->assertFalse($outing->getLimitSubscriptionDate() === $dateFalse);
+        $this->assertFalse($outing->getMaxUsers() === 25);
+        $this->assertFalse($outing->getAbout() === '8 jours au bistrot, oui c\'est long');
+    }
+}
