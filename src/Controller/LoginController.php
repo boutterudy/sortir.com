214b545f76546b5490d3875d0e98da95e0ce6fdd
Rudy B <btte.rudy@gmail.com>
2022-01-24 15:56:37 +0100

    Added authentication system, including remember me option

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        if(!$this->getUser() instanceof User) {
+            // Get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+
+            // Last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+            return $this->render('login/index.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]);
+        }
+
+        return $this->redirectToRoute('temporary');
+
+    }
+}
