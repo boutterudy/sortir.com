0a447de7f510ede5a61c4ea748db06f4b8da4d5b
ErikLesteven <eriklesteven@gmail.com>
2022-02-01 14:41:34 +0100

    Outing Update, ongoing

---
+++ src/Controller/OutingUpdateController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Outing;
+use App\Form\OutingType;
+use App\Repository\OutingRepository;
+use App\Repository\StatusRepository;
+use App\Repository\TownRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OutingUpdateController extends AbstractController
+{
+    /**
+     * @Route("/sortie/modification/{idOuting}", name="outing_update", requirements={"idOuting"="\d+"})
+     */
+    public function update(Request                $request,
+                           UserRepository         $userRepository,
+                           StatusRepository       $statusRepository,
+                           OutingRepository       $outingRepository,
+                           EntityManagerInterface $manager,
+                                                  $idOuting): Response
+    {
+        $outing = $outingRepository->find($idOuting);
+        $user = $outing->getOrganizer();
+
+        //TODO
+        if($user !== $userRepository->find($this->getUser()->getId()) && !$user->getIsAdmin()){
+            //$this->redirect("");
+        }
+
+        $outingUpdateForm = $this->createForm(OutingType::class, $outing);
+
+        $outingUpdateForm->handleRequest($request);
+
+        if ($outingUpdateForm->isSubmitted()) {
+            if($outingUpdateForm->get('save')->isClicked()){
+                $outing->setStatus($statusRepository->findOneBy(['libelle'=>'En création']));
+            }elseif($outingUpdateForm->get('publish')->isClicked()) {
+                $outing->setStatus($statusRepository->findOneBy(['libelle'=>'Ouverte']));
+            }elseif($outingUpdateForm->get('suppress')->isClicked()) {
+                $outing->setStatus($statusRepository->findOneBy(['libelle'=>'Annulée']));
+            }
+            $manager->persist($outing);
+            $manager->flush();
+
+            //TODO
+            if($outingUpdateForm->get('suppress')->isClicked()){
+                //$this->redirect();
+            }
+        }
+
+        return $this->render('outing_update/outing_update.html.twig', [
+            'controller_name' => 'OutingUpdateController',
+            'outingCreationForm' => $outingUpdateForm->createView(),
+            'outing' => $outing
+        ]);
+    }
+}
